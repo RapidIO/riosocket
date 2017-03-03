f16b3a0cb3d161b64017e64565ec3a921864b489
Alexandre Bounine <alexandre.bounine@idt.com>
2017-03-03 15:16:16 -0500

    Add parameters used by interleaved DMA transfers
    Release v1.1.0 of "kernel-rapidio" driver package contains changes to
    support Tsi721 interleaved DMA transfers.
    This patch sets new parameters in tx_data structure to 0.

--- riosocket-basic/riosocket-riodev.c
+++ riosocket-basic/riosocket-riodev.c
@@ -200,6 +200,10 @@ static int riosocket_dma_packet( struct riosocket_node *node, struct sk_buff *sk
 	tx_data.rio_addr_u = 0;
 	tx_data.rio_addr = rioaddr;
 	tx_data.wr_type = RDW_ALL_NWRITE;
+	tx_data.ssdist = 0;
+	tx_data.sssize = 0;
+	tx_data.dsdist = 0;
+	tx_data.dssize = 0;
 
 	flags = DMA_CTRL_ACK | DMA_PREP_INTERRUPT;
 
